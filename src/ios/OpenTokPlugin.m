65f9ffa36e2c6181a21e2cd03173b97d3656814b
Marco Palmisano <marco.palmisano@crispybacon.it>
2020-12-11 11:48:26 +0100

    Updated dependencies

--- src/ios/OpenTokPlugin.m
+++ src/ios/OpenTokPlugin.m
@@ -57,8 +57,8 @@
     [payload setObject:@"3.4.3" forKey:@"cp_version"];
     NSMutableDictionary *logData = [[NSMutableDictionary alloc]init];
     [logData setObject:apiKey forKey:@"partner_id"];
-    [logData setObject:@"2.16.6" forKey:@"build"];
-    [logData setObject:@"https://github.com/opentok/cordova-plugin-opentok" forKey:@"source"];
+    [logData setObject:@"2.18.1" forKey:@"build"];
+    [logData setObject:@"https://github.com/ziomarco/cordova-plugin-opentok" forKey:@"source"];
     [logData setObject:@"info" forKey:@"payload_type"];
     [logData setObject:payload forKey:@"payload"];
     [logData setObject:sessionId forKey:@"session_id"];
